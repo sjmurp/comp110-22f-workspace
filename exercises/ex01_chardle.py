b73f549a96f93fd6630bf51e5360a7c7677565f6
Sean Murphy <sjmurp@ad.unc.edu>
2022-08-28 19:38:34 -0400

    fiixed gradesope issues

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -17,26 +17,25 @@ print("Searching for " + letter + " in " + word)
 
 if word[0] == letter:
     print(letter + " found at index 0")
-    counter+=1   
+    counter += 1   
 
 if word[1] == letter:
     print(letter + " found at index 1")
-    counter+=1
+    counter += 1
 
 if word[2] == letter:
     print(letter + " found at index 2")
-    counter+=1
+    counter += 1
 
 if word[3] == letter:
     print(letter + " found at index 3")
-    counter+=1
+    counter += 1
 
 if word[4] == letter:
     print(letter + " found at index 4")
-    counter+=1
+    counter += 1
 
 if counter > 0:
     print(str(counter) + " instances of " + letter + " found in " + word)
 else:
     print("No instances of " + letter + " found in " + word)
-
